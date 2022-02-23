01473ec1a41a03302963db6d13f08503d7de51c6
Henry-idiotium <quocdat.381872@gmail.com>
2022-02-23 22:39:17 +0700

    chore: :pencil2: Rename and update several response message classes

--- UIM.Common/ResponseMessages/ErrorResponseMessages.cs
+++ UIM.Common/ResponseMessages/ErrorResponseMessages.cs
@@ -2,13 +2,14 @@ namespace UIM.Common.ResponseMessages
 {
     public static class ErrorResponseMessages
     {
-        public const string FailedLogin = "Email or password is incorrect.";
-        public const string BadRequest = "Invalid request.";
-        public const string Forbidden = "Request rejected.";
-        public const string PasswordNotMatch = "Submitted passwords didn't match.";
-        public const string Unauthenticated = "Failed to authenticate with this resource.";
-        public const string Unauthorized = "Unauthorized to access this resource.";
-        public const string UnexpectedError = "Something went wrong.";
-        public const string UserAlreadyExists = "This username or email already exists.";
+        public const string BadRequest = "Invalid request";
+        public const string FailedLogin = "Email or password is incorrect";
+        public const string FailedResetPassword = "Failed to reset password";
+        public const string Forbidden = "Request rejected";
+        public const string IncorrectOldPassword = "The old password is incorrect";
+        public const string Unauthenticated = "Failed to authenticate with this resource";
+        public const string Unauthorized = "Unauthorized to access this resource";
+        public const string UnexpectedError = "Something went wrong";
+        public const string UserAlreadyExists = "This username or email already exists";
     }
 }
--- UIM.Common/ResponseMessages/SuccessResponseMessages.cs
+++ UIM.Common/ResponseMessages/SuccessResponseMessages.cs
@@ -3,7 +3,7 @@ namespace UIM.Common.ResponseMessages
     public static class SuccessResponseMessages
     {
         public const string RequestSucceeded = "Request succeeded";
-        public const string TokenRevoked = "Token's revoked";
-        public const string EmailVerified = "Email's verified";
+        public const string UserRegistered = "User registered";
+        public const string TokenRevoked = "Token revoked";
     }
 }
