065210f014e619c39da110740a0d1eea6bae82ad
Henry-idiotium <quocdat.381872@gmail.com>
2022-03-22 10:26:00 +0700

    chore: :card_file_box: Update Name prop with unique attribute for entity Tag and Deparment

---
+++ UIM.Core/Data/Migrations/20220322031752_Tag_Department_UpdateNamePropWithUniqueAttribute.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UIM.Core.Data.Migrations
+{
+    public partial class Tag_Department_UpdateNamePropWithUniqueAttribute : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Departments",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tags_Name",
+                table: "Tags",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_Name",
+                table: "Departments",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Tags_Name",
+                table: "Tags");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departments_Name",
+                table: "Departments");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Departments",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
