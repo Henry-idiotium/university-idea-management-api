3da8602cbbec2679dbeb70a78c01a50e4261431d
Henry-idiotium <quocdat.381872@gmail.com>
2022-03-25 09:35:33 +0700

    chore: :adhesive_bandage: Move AddTag methods from Idea to Submission

--- UIM.Core/Data/Repositories/Interfaces/IIdeaRepository.cs
+++ UIM.Core/Data/Repositories/Interfaces/IIdeaRepository.cs
@@ -2,6 +2,5 @@ namespace UIM.Core.Data.Repositories.Interfaces;
 
 public interface IIdeaRepository : IRepository<Idea>
 {
-    Task<bool> AddToTagAsync(Idea idea, Tag tag);
     Task<IEnumerable<Idea>> GetBySubmissionAsync(string submissionId);
 }
--- UIM.Core/Data/Repositories/Interfaces/ISubmissionRepository.cs
+++ UIM.Core/Data/Repositories/Interfaces/ISubmissionRepository.cs
@@ -2,4 +2,5 @@ namespace UIM.Core.Data.Repositories.Interfaces;
 
 public interface ISubmissionRepository : IRepository<Submission>
 {
+    Task<bool> AddToTagAsync(Submission idea, Tag tag);
 }
