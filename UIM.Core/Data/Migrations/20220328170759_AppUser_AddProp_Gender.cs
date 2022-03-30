6b6364898f13aa243a6f61d61702f7a031ce49f0
Henry-idiotium <quocdat.381872@gmail.com>
2022-03-30 07:48:47 +0700

    chore: :card_file_box: Add prop Gender to AppUser

---
+++ UIM.Core/Data/Migrations/20220328170759_AppUser_AddProp_Gender.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UIM.Core.Data.Migrations
+{
+    public partial class AppUser_AddProp_Gender : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "nvarchar(10)",
+                maxLength: 10,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+        }
+    }
+}
